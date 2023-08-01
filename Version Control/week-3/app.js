fb49c4a95ab49f321ebd453eaeb43a4abe0b0b25
akashydv311 <akashydv311@gmail.com>
2023-08-01 14:46:56 +0530

    swape code in app.js

--- Version Control/week-3/app.js
+++ Version Control/week-3/app.js
@@ -1,6 +1,11 @@
 console.log("Let's make the swape code in js");
 
-const swapNumber = (a, b)=>{
-	return [b, a];
+const swapNumber = (arr)=>{
+	return [arr[1], arr[0]];
 }
 
+const arr=[12,34];
+
+console.log("Given value => ", arr);
+console.log("Type of return value => ", typeof swapNumber(arr));
+console.log("Ans is => ", swapNumber(arr));
